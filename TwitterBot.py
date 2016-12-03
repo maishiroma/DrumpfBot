ed1e6c1cabed71d6559623719454767ebfd61277
britt103 <britt103@mail.chapman.edu>
2016-12-02 18:13:11 -0800

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -153,8 +153,8 @@ def retweet():
             publish = False
         else:
             for i in range(0, 16): ## iterate through 16 times to get max No. of tweets
-                name = twitter_api.account_settings('screen_name')
-                user_timeline = twitter_api.statuses_user_timeline(screen_name=name,count=200)
+                name = twitter_api.account_settings()
+                user_timeline = twitter_api.statuses_user_timeline(screen_name=name.get('screen_name'),count=200)
                 for tweet in user_timeline:
                     if (tweet.get('id') == new_tweet.get('id')):
                         publish = False
