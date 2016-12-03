161a8baa446f5cfdb69dfdb695fd3859a8803668
britt103 <britt103@mail.chapman.edu>
2016-12-02 17:53:44 -0800

    Update TwitterBot.py

--- TwitterBot.py
+++ TwitterBot.py
@@ -4,7 +4,6 @@
 # https://github.com/sixohsix/twitter for the type of API we're using
 # Authors: Matthew Shiroma and Ryan Britton
 # Version 1.0
-from twython import Twython
 import twitter
 import logging
 import json
